3050d0eb8ad9985d1a7b71a17fa9a20de0efac55
millalin <milla.lintunen@hotmail.com>
2020-03-31 17:08:20 +0300

    backend token

--- task-launch-panel/src/jenkinsService.ts
+++ task-launch-panel/src/jenkinsService.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const token = process.env.BACKTOKEN;
+
 const URL = 'http://localhost:4000';
 
 const getAllJobs = async () => {
@@ -7,8 +9,13 @@ const getAllJobs = async () => {
   return res.data;
 };
 
+
+
 const build = async (job: any, parameters: any, branch: any = 'master') => {
-  const res = await axios.post(`${URL}/build/${job}/${branch}`, parameters);
+  const res = await axios.post(`${URL}/build/${job}/${branch}`, {
+    parameters: parameters,
+    token: token
+  });
   return res.data;
 };
 
